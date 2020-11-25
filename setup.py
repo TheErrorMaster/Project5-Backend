4668f49f44e963a3dc59eb71c136aef21bc8b4f8
TheErrorMaster <61210645+TheErrorMaster@users.noreply.github.com>
2020-11-25 15:12:14 -0800

    first

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='CPSC449-Project2',
+    version='1.0.1',
+    author='Andrew Dinh',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['Flask']
+)
